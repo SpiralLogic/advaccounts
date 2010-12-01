7b4be30ac3d48bc1db5563607ac69303c0ca3e45
admin <admin@9277f663-478f-4daf-a1d3-754d7f1cc17a>
2010-12-01 13:48:03 +0000

    fucking with menus and crap
    git-svn-id: https://advanced.sorijen.net.au:2223/svn/accounts@94 9277f663-478f-4daf-a1d3-754d7f1cc17a

--- index2.php
+++ index2.php
@@ -1,31 +1,21 @@
 <head>
 	<script src='js/jquery-1.4.3.min.js'></script>
+
 	<script> function gomango() {
 		alert("Phil's advice for the day " + $("#InputSomething").val())
 	}</script>
 </head>
 		<?php
-
+require_once('FirePHPCore/fb.php');
+require_once("includes/firephp.php");
 include_once('includes/ui2/ui.inc');
-
-
-
-
-UI::body('body', array('style' => 'background-color: royalblue'));
-
-UI::divStart('HiPhil', array('style' => 'margin:20px auto; text-align:center; font-weight:bold;font-size:40px'));
-echo "Hi Phil";
-UI::table('start', array('style' => 'width:80%; text-align:center; margin:0 auto;'));
-UI::tr('start')->tdStart('left', array('style' => 'width:50%'));
-
+UI::body('page', array('style' => 'background-color: royalblue'),true);
+UI::div('HiPhil', array('style' => 'margin:20px auto; text-align:center; font-weight:bold;font-size:40px'), true);
+UI::table('table1', array('style' => 'width:80%; text-align:center; margin:0 auto;'), true)->tr(true)->td('left', array('style' => 'width:50%'), true);
 
 UI::img('pic1', array('src' => 'http://images.apple.com/iphone/features/images/homescreen-hero-20101116.png', 'alt' => 'Wawa?', 'title' => 'Wawa'));
 
-
-UI::tdEnd()->tdStart('right', array('style' => 'width:50%'));
-
-UI::select('1', array('The first fuckingthing' => 0, 'wrong choice' => 1))->br()->input('InputSomething', array('name' => 'input1', 'length' => 15))->br()->button('button', 'i am a button',
-	'button1', 'whatever', 'boring', 'gobacktoschool', array('onclick' => 'javascript:gomango()'))->br();
-UI::tdEnd()->tr('end')->table('end')->divEnd();
-
+UI::td(false)->td('right', array('style' => 'width:50%'),true);
+UI::button('button1', 'i am a button')->td(false)->tr(false);
+UI::div(false)->body(false);
 
