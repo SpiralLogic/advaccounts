683fe961f70de790eca8db87f8c52e7232f18337
admin <admin@9277f663-478f-4daf-a1d3-754d7f1cc17a>
2011-05-10 07:59:51 +0000

    adding js upload drag and drop
    git-svn-id: https://advanced.sorijen.net.au:2223/svn/accounts@168 9277f663-478f-4daf-a1d3-754d7f1cc17a

--- index2.php
+++ index2.php
@@ -1,29 +1,16 @@
 <!DOCTYPE html>
 
 		<?php
-  include('includes/ui2/html.inc');
-/* mysql_pconnect('localhost','fa','1willenberg');
-mysql_select_db('fa');
-$sql = "SHOW tables";
-$rs = mysql_query($sql);
-while ($row = mysql_fetch_array($rs))
-{
-	$tbl = $row[0];
-	if ($tbl=='0_debtor_trans' || $tbl=='0_stock_master' || $tbl=='0_debtor_trans_view') continue;
-	$sql = "ALTER TABLE $tbl  ENGINE=INNODB";
-	mysql_query($sql);
-}*/
-HTML::html(true);
-HTML::head(true)->meta(array('charset' => 'UTF-8'));
-HTML::title(true);
-echo 'Hi Syed!';
-HTML::title()->head;
-HTML::table(true)->tr(true)->td(true);
-echo "dsgsgs";
-HTML::td()->tr->tr(true)->td(true);
-echo "sfdgsrfg";
-HTML::td()->tr->table;
-HTML::img('teetst', array('src' => 'http://www.w3schools.com/images/w3schoolslogo.gif'));
+$path_to_root='./';
+	include_once('includes/session.inc');
+	$sql = "CREATE TABLE upload(id INT NOT NULL AUTO_INCREMENT,
+filename VARCHAR(30) NOT NULL,
+type VARCHAR(30) NOT NULL,
+size INT NOT NULL,
+content MEDIUMBLOB NOT NULL,
+PRIMARY KEY(id)
+)";
+	db_query($sql, 'fucked the job');
 
-HTML::html();
 
+	?>
