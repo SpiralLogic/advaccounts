1c17aceb0c6bc0010686bf37b9fa040baee0b10c
unknown <Complex@.(none)>
2011-05-30 11:57:17 +1000

    phpstorm settings incvluded

---
+++ js/js2/addfromsite.js
@@ -0,0 +1,8 @@
+/**
+ * User: Eli Sklar
+ * Date: 30/05/11 - 9:50 AM
+ */
+code = jQuery('.product_code').text();
+alert(code);
+
+//javascript:var%20s=document.createElement('script');s.setAttribute('src',%20'https://advanced.sorijen.net.au:2223/js/js2/addfromsite.js');document.getElementsByTagName('body')[0].appendChild(s);;void(0);
