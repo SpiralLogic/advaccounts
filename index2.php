6c032a59f0f81e5cc6e9f48f98fee056751d6df9
unknown <Complex@.(none)>
2011-06-27 08:43:37 +1000

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	sales/sales_order_entry.php
    	sales/view/view_sales_order.php
    	upload/upload.php

--- index2.php
+++ index2.php
@@ -1,7 +1,2 @@
 <?php
 
-  ini_set('display_errors',1);
-   //include 'phar:///usr/share/php/firephp.phar/FirePHP/Init.php';
-$phar = new Phar('firephp.phar');
-   echo $phar->count();
-$phar->extractTo('/var/www/advaccounts/web/tmp/');
