264299ed4fbd20d7a03ca2b3bf171a16bc8ae747
unknown <advanced@.(none)>
2011-07-20 15:14:53 +1000

    reverted master to current release

--- inventory/reorder_level.php
+++ inventory/reorder_level.php
@@ -1,17 +1,17 @@
 <?php
 /**********************************************************************
-    Copyright (C) FrontAccounting, LLC.
-	Released under the terms of the GNU General Public License, GPL, 
-	as published by the Free Software Foundation, either version 3 
-	of the License, or (at your option) any later version.
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
-    See the License here <http://www.gnu.org/licenses/gpl-3.0.html>.
-***********************************************************************/
+Copyright (C) FrontAccounting, LLC.
+Released under the terms of the GNU General Public License, GPL,
+as published by the Free Software Foundation, either version 3
+of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the License here <http://www.gnu.org/licenses/gpl-3.0.html>.
+ ***********************************************************************/
 $page_security = 'SA_REORDER';
 $path_to_root = "..";
-include_once($path_to_root . "/includes/session.inc");
+include_once($_SERVER['DOCUMENT_ROOT'] . "/includes/session.inc");
 
 page(_($help_context = "Reorder Levels"));
 
@@ -28,8 +28,7 @@ check_db_has_costable_items(_("There are no inventory items defined in the syste
 if (isset($_GET['stock_id']))
 	$_POST['stock_id'] = $_GET['stock_id'];
 
-if (list_updated('stock_id')) 
-{
+if (list_updated('stock_id')) {
 	$Ajax->activate('show_heading');
 	$Ajax->activate('reorders');
 }
@@ -40,7 +39,7 @@ start_form();
 if (!isset($_POST['stock_id']))
 	$_POST['stock_id'] = get_global_stock_item();
 
-echo "<center>" . _("Item:"). "&nbsp;";
+echo "<center>" . _("Item:") . "&nbsp;";
 echo stock_costable_items_list('stock_id', $_POST['stock_id'], false, true);
 
 echo "<hr></center>";
@@ -52,13 +51,13 @@ div_end();
 set_global_stock_item($_POST['stock_id']);
 
 div_start('reorders');
-start_table(TABLESTYLE, "width=30%");
+start_table("$table_style width=30%");
 
 $th = array(_("Location"), _("Quantity On Hand"), _("Re-Order Level"));
 table_header($th);
 
 $j = 1;
-$k=0; //row colour counter
+$k = 0; //row colour counter
 
 $result = get_loc_details($_POST['stock_id']);
 
@@ -67,11 +66,9 @@ while ($myrow = db_fetch($result))
 
 	alt_table_row_color($k);
 
-	if (isset($_POST['UpdateData']) && check_num($myrow["loc_code"]))
-	{
+	if (isset($_POST['UpdateData']) && check_num($myrow["loc_code"])) {
 
 		$myrow["reorder_level"] = input_num($myrow["loc_code"]);
-		FB::info($myrow['reorder_level']);
 		set_reorder_level($_POST['stock_id'], $myrow["loc_code"], input_num($myrow["loc_code"]));
 		display_notification(_("Reorder levels has been updated."));
 	}
@@ -83,11 +80,10 @@ while ($myrow = db_fetch($result))
 	$_POST[$myrow["loc_code"]] = qty_format($myrow["reorder_level"], $_POST['stock_id'], $dec);
 
 	qty_cell($qoh, false, $dec);
-	qty_cells(null, $myrow["loc_code"], is_numeric(input_num($myrow["loc_code"])) ? input_num($myrow["loc_code"]) : 0 , null, null, $dec);
+	qty_cells(null, $myrow["loc_code"], null, null, null, $dec);
 	end_row();
 	$j++;
-	If ($j == 12)
-	{
+	If ($j == 12) {
 		$j = 1;
 		table_header($th);
 	}
