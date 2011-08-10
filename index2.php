55051e8a4755a9ec921d44aa27d3972e52987c36
Advanced <admin@advancedgroup.com.au>
2011-08-11 00:16:43 +1000

    making focus shit work

--- index2.php
+++ index2.php
@@ -1,7 +1,7 @@
 <?php
 /*
 	include('config_db.php');
-	include('includes/dbpdo.inc');
+	include('includes/db.inc');
 	ini_set('display_errors', 'on');
 	DB::initConnect($db_connections[0]['dbname'], $db_connections[0]['dbuser'], $db_connections[0]['dbpassword']);
 	$result = DB::prepare("SELECT * FROM users WHERE real_name LIKE :name");
