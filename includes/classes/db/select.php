062585d4528a0b764cf9f30fec371f1e33464bef
unknown <Complex@.(none)>
2011-08-18 05:05:07 +1000

    dbo coolneess2

--- includes/classes/db/select.php
+++ includes/classes/db/select.php
@@ -7,56 +7,56 @@
  * To change this template use File | Settings | File Templates.
  */
 
-	class Select extends Query {
+  class Select extends Query {
 
-		protected $select = array();
-		protected $from = array();
-		protected $limit = array();
-		protected $orderby = array();
-		protected $groupby = array();
+	 protected $select = array();
+	 protected $from = array();
+	 protected $limit = array();
+	 protected $orderby = array();
+	 protected $groupby = array();
 
-		public function select($column = null, $orderby = false, $groupby = false) {
-			if ($column) $this->select[] = $column;
-			if ($orderby) $this->orderby($column, ($orderby != 'desc') ? false : true);
-			if ($groupby) $this->groupby($column);
-			return $this;
-		}
+	 public function select($column = null, $orderby = false, $groupby = false) {
+		if ($column) $this->select[] = $column;
+		if ($orderby) $this->orderby($column, ($orderby != 'desc') ? false : true);
+		if ($groupby) $this->groupby($column);
+		return $this;
+	 }
 
-		public function from($table) {
-			$this->from[] = $table;
-			return $this;
-		}
+	 public function from($table) {
+		$this->from[] = $table;
+		return $this;
+	 }
 
-		function orderby($by, $asc = true) {
-			if (!$asc) $by = $by . ' DESC';
-			$this->orderby[] = $by;
-			return $this;
-		}
+	 function orderby($by, $asc = true) {
+		if (!$asc) $by = $by . ' DESC';
+		$this->orderby[] = $by;
+		return $this;
+	 }
 
-		function groupby($by) {
-			$this->groupby[] = $by;
-			return $this;
-		}
+	 function groupby($by) {
+		$this->groupby[] = $by;
+		return $this;
+	 }
 
-		public function limit($start, $quantity) {
-			$this->limit = array($start, $quantity);
-			return $this;
-		}
+	 public function limit($start, $quantity) {
+		$this->limit = array($start, $quantity);
+		return $this;
+	 }
 
-		public function exec($data) {
-			return $this->_buildQuery();
-		}
+	 public function exec($data) {
+		return $this->_buildQuery();
+	 }
 
-		protected function _buildQuery() {
-			$sql = "SELECT ";
-			$sql .= (empty($this->select)) ? '*' : implode(', ', $this->select);
-			$sql .= " FROM " . implode(', ', $this->from);
-			$sql .= parent::_buildQuery();
+	 protected function _buildQuery() {
+		$sql = "SELECT ";
+		$sql .= (empty($this->select)) ? '*' : implode(', ', $this->select);
+		$sql .= " FROM " . implode(', ', $this->from);
+		$sql .= parent::_buildQuery();
 
-			if (!empty($this->orderby)) $sql .= ' ORDER BY ' . implode(', ', $this->orderby);
-			if (!empty($this->groupby)) $sql .= ' GROUP BY ' . implode(', ', $this->groupby);
-			return $sql;
-		}
+		if (!empty($this->orderby)) $sql .= ' ORDER BY ' . implode(', ', $this->orderby);
+		if (!empty($this->groupby)) $sql .= ' GROUP BY ' . implode(', ', $this->groupby);
+		return $sql;
+	 }
 
-	}
+  }
 
