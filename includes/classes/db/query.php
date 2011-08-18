0da4e2a0f67bdbefa92883c99137c2029d038809
Advanced <admin@advancedgroup.com.au>
2011-08-18 18:04:44 +1000

    shit

--- includes/classes/db/query.php
+++ includes/classes/db/query.php
@@ -7,63 +7,63 @@
  * To change this template use File | Settings | File Templates.
  */
 
-	abstract class Query {
+  abstract class Query {
 
-		protected $insert = array();
-		protected $where = array();
-		public $data = array();
-		protected $count = 0;
+	 protected $insert = array();
+	 protected $where = array();
+	 public $data = array();
+	 protected $count = 0;
 
-		protected function _where($condition, $type = 'AND', $uservar) {
-			if (is_array($condition)) {
-				foreach ($condition as $condition => $uservar) $this->_where($condition, $type, $uservar);
-				return $this;
-			}
-			if ($uservar !== null) {
-				$name = ':dbcondition' . $this->count;
-				$this->count++;
-				$this->data[$name] = $uservar;
-				$condition = $condition . ' ' . $name;
-			}
-			$this->where[] = (empty($this->where)) ? $condition : $type . ' ' . $condition;
-			return $this;
+	 protected function _where($condition, $type = 'AND', $uservar) {
+		if (is_array($condition)) {
+		  foreach ($condition as $condition => $uservar) $this->_where($condition, $type, $uservar);
+		  return $this;
 		}
-
-		public function where($condition, $uservar = null) {
-			return $this->_where($condition, '', $uservar);
+		if ($uservar !== null) {
+		  $name = ':dbcondition' . $this->count;
+		  $this->count++;
+		  $this->data[$name] = $uservar;
+		  $condition = $condition . ' ' . $name;
 		}
+		$this->where[] = (empty($this->where)) ? $condition : $type . ' ' . $condition;
+		return $this;
+	 }
 
-		public function or_where($condition, $uservar = null) {
-			return $this->_where($condition, 'OR', $uservar);
-		}
+	 public function where($condition, $uservar = null) {
+		return $this->_where($condition, '', $uservar);
+	 }
 
-		public function and_where($condition, $uservar = null) {
-			return $this->_where($condition, 'AND', $uservar);
-		}
+	 public function or_where($condition, $uservar = null) {
+		return $this->_where($condition, 'OR', $uservar);
+	 }
 
-		public function or_open($condition, $uservar = null) {
-			return $this->_where($condition, 'OR (', $uservar);
-		}
+	 public function and_where($condition, $uservar = null) {
+		return $this->_where($condition, 'AND', $uservar);
+	 }
 
-		public function and_open($condition, $uservar = null) {
-			return $this->_where($condition, 'AND (', $uservar);
-		}
+	 public function or_open($condition, $uservar = null) {
+		return $this->_where($condition, 'OR (', $uservar);
+	 }
 
-		public function close_and($condition, $uservar = null) {
-			return $this->_where($condition, ') AND', $uservar);
-		}
+	 public function and_open($condition, $uservar = null) {
+		return $this->_where($condition, 'AND (', $uservar);
+	 }
 
-		public function close_or($condition, $uservar = null) {
-			return $this->_where($condition, ') OR', $uservar);
-		}
+	 public function close_and($condition, $uservar = null) {
+		return $this->_where($condition, ') AND', $uservar);
+	 }
 
-		abstract public function exec($data);
+	 public function close_or($condition, $uservar = null) {
+		return $this->_where($condition, ') OR', $uservar);
+	 }
 
-		protected function _buildQuery() {
-			$sql = '';
-			if (!empty($this->where)) $sql .= ' WHERE ' . implode(' ', $this->where);
-			$this->where=array();
-			return $sql;
-		}
+	 abstract public function exec($data);
 
-	}
+	 protected function _buildQuery() {
+		$sql = '';
+		if (!empty($this->where)) $sql .= ' WHERE ' . implode(' ', $this->where);
+		$this->where = array();
+		return $sql;
+	 }
+
+  }
--- includes/classes/db/select.php
+++ includes/classes/db/select.php
@@ -7,71 +7,71 @@
  * To change this template use File | Settings | File Templates.
  */
 
-	class Select extends Query {
+  class Select extends Query {
 
-		protected $select = array();
-		protected $from = array();
-		protected $limit = array();
-		protected $orderby = array();
-		protected $groupby = array();
-		protected $union = array();
+	 protected $select = array();
+	 protected $from = array();
+	 protected $limit = array();
+	 protected $orderby = array();
+	 protected $groupby = array();
+	 protected $union = array();
 
-		public function select($column = null, $orderby = false, $groupby = false) {
-			if (is_array($column)) foreach ($column as $c) call_user_func_array(array($this, 'select'), (array)$c);
-			else {
-				$order = explode('as', $column);
-				if ($column) $this->select[] = $column;
-				if ($orderby) $this->orderby($order[0], ($orderby != 'desc') ? false : true);
-				if ($groupby) $this->groupby($order[0]);
-			}
-			return $this;
+	 public function select($column = null, $orderby = false, $groupby = false) {
+		if (is_array($column)) foreach ($column as $c) call_user_func_array(array($this, 'select'), (array)$c);
+		else {
+		  $order = explode('as', $column);
+		  if ($column) $this->select[] = $column;
+		  if ($orderby) $this->orderby($order[0], ($orderby != 'desc') ? false : true);
+		  if ($groupby) $this->groupby($order[0]);
 		}
+		return $this;
+	 }
 
-		public function from($table) {
-			$this->from[] = $table;
-			return $this;
-		}
+	 public function from($table) {
+		$this->from[] = $table;
+		return $this;
+	 }
 
-		function orderby($by, $asc = true) {
-			if (!$asc) $by = $by . ' DESC';
-			$this->orderby[] = $by;
-			return $this;
-		}
+	 function orderby($by, $asc = true) {
+		if (!$asc) $by = $by . ' DESC';
+		$this->orderby[] = $by;
+		return $this;
+	 }
 
-		function groupby($by) {
-			$this->groupby[] = $by;
-			return $this;
-		}
+	 function groupby($by) {
+		$this->groupby[] = $by;
+		return $this;
+	 }
 
-		public function limit($start, $quantity = null) {
-			$this->limit = ($quantity == null) ? $start : "$start, $quantity";
-			return $this;
-		}
+	 public function limit($start, $quantity = null) {
+		$this->limit = ($quantity == null) ? $start : "$start, $quantity";
+		return $this;
+	 }
 
-		public function union() {
-			$this->union[] = '(' . $this->_buildQuery() . ')';
-			$this->select = $this->from = $this->orderby = $this->groupby = array();
-			$this->limit = '';
-			return $this;
-		}
+	 public function union() {
+		$this->union[] = '(' . $this->_buildQuery() . ')';
+		$this->select = $this->from = $this->orderby = $this->groupby = array();
+		$this->limit = '';
+		return $this;
+	 }
 
-		public function exec($data) {
-			if ($this->union) return implode(' UNION ', $this->union);
-			return $this->_buildQuery();
-		}
+	 public function exec($data) {
+		if ($this->union) return implode(' UNION ', $this->union);
+		return $this->_buildQuery();
+	 }
 
-		protected function _buildQuery() {
+	 protected function _buildQuery() {
 
-			$sql = "SELECT ";
-			$sql .= (empty($this->select)) ? '*' : implode(', ', $this->select);
-			$sql .= " FROM " . implode(', ', $this->from);
-			$sql .= parent::_buildQuery();
+		$sql = "SELECT ";
+		$sql .= (empty($this->select)) ? '*' : implode(', ', $this->select);
+		$sql .= " FROM " . implode(', ', $this->from);
+		$sql .= parent::_buildQuery();
 
-			if (!empty($this->orderby)) $sql .= ' ORDER BY ' . implode(', ', $this->orderby);
-			if (!empty($this->groupby)) $sql .= ' GROUP BY ' . implode(', ', $this->groupby);
-			if (!empty($this->limit)) $sql .= ' LIMIT ' . $this->limit;
-			return $sql;
-		}
+		if (!empty($this->orderby)) $sql .= ' ORDER BY ' . implode(', ', $this->orderby);
+		if (!empty($this->groupby)) $sql .= ' GROUP BY ' . implode(', ', $this->groupby);
+		if (!empty($this->limit)) $sql .= ' LIMIT ' . $this->limit;
+		return $sql;
+	 }
 
-	}
+  }
 
