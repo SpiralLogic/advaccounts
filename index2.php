78a316b3f6eddc4a02bdd8e9a515a47295cd8b68
Advanced <admin@advancedgroup.com.au>
2011-08-19 16:53:32 +1000

    readded draggable

--- index2.php
+++ index2.php
@@ -4,10 +4,10 @@
   ini_set('display_errors', 'On');
   error_reporting(E_ALL);
   echo "<pre>";
-  $terms = 'test';
+  /*$terms = 'test';
   DB::select(array('debtor_no as id', array('name as label', true), 'name as value'))->from('debtors_master')->where('name LIKE ', "$terms%")->limit(20)
 	->union()->select(array('debtor_no as id', 'name as label', 'name as value'))->from('debtors_master')->where('debtor_ref LIKE', "%$terms%")
 	->or_where('name LIKE', "%$terms%")->or_where('debtor_no LIKE', "%$terms%")->limit(20)->union();
-  $test = DB::fetchAll();
+  $test = DB::fetchAll();*/
 
   var_dump($test);
