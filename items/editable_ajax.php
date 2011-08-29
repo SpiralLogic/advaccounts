d1c71b2e2fa08d4d7cae851b39f85bda1189b71d
Advanced <admin@advancedgroup.com.au>
2011-08-29 20:45:54 +1000

    pdo  almost finished

---
+++ items/editable_ajax.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: advanced
+ * Date: 29/08/11
+ * Time: 3:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+	include('../includes/session.inc');
+
+	ini_set('display_errors', 'On');
+	error_reporting(E_ALL);
+	$item = array('id' => $_POST['row_id']);
+	$q = DB::update('stock')->where('id=', $item['id']);
+	switch ($_POST['column']) {
+		case 0:
+			$f = 'id';
+			break;
+		case 1:
+			$f = 'name';
+			break;
+		case 2:
+			$f = 'description';
+			break;
+		case 3:
+			$f = 'price';
+			break;
+	}
+	$item[$f] = $_POST['value'];
+
+
+	echo	$q->exec($item);
