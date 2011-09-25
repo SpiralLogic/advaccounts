7751eadff2248822c9e0e68997494819867b839e
Advanced <admin@advancedgroup.com.au>
2011-09-26 09:32:09 +1000

    new storm, new config and shit

--- items/includes/js/items.js
+++ items/includes/js/items.js
@@ -6,17 +6,15 @@
  * To change this template use File | Settings | File Templates.
  */
 (function(window, undefined) {
-	var Adv = window.Adv,
-	 ajaxRequest,
-	 Items = {};
+	var Adv = window.Adv, ajaxRequest, Items = {};
 	(function() {
-		var $this = this,item,term,results,itemList = $('#itemList'),itemDetails = $("#itemDetails");
+		var $this = this, item, term, results, itemList = $('#itemList'), itemDetails = $("#itemDetails");
 		itemList.delegate('span', 'click', function() {
 			$this.getItem($(this).attr('id'));
 			$(this).remove();
 		});
-		this.getItem = function (id) {
-			$.post("items.php", {id: id}, function(data) {
+		this.getItem = function(id) {
+			$.post("items.php", {id:id}, function(data) {
 				var content = $('<td><td/>').css('vertical-align', 'top');
 				item = {stock_id:data.stock_id,
 					description:data.description,
@@ -41,7 +39,7 @@
 		};
 		this.addFeild = function(name, value) {
 
-			var input = (name == 'description' || name == 'long_description') ? $('<textarea>').attr({'name':name}).text(value) : $('<input/>').attr({'name':name,value:value});
+			var input = (name == 'description' || name == 'long_description') ? $('<textarea>').attr({'name':name}).text(value) : $('<input/>').attr({'name':name, value:value});
 			if (name == 'inactive' || name == 'no_sale') input.attr('type', 'checkbox');
 			return input;
 
@@ -52,60 +50,60 @@
 
 $(function($) {
 	var oTable = $('#itemDetails').dataTable({
-																						 "bProcessing": true,
-																						 "bServerSide": true,
-																						 "bJQueryUI": true,
-																						 "iDisplayLength": 100,
-																						 "sAjaxSource": "includes/server_processing.php",
-																						 "aoColumns": [
-																							 {"sClass":"editable"},
+																						 "bProcessing":true,
+																						 "bServerSide":true,
+																						 "bJQueryUI":true,
+																						 "iDisplayLength":100,
+																						 "sAjaxSource":"includes/server_processing.php",
+																						 "aoColumns":[
+																							 {},
 																							 {"sClass":"editable_textarea"},
 																							 {"sClass":"editable_textarea"},
-																							 {"sClass":"editable","sType":"numeric"},
+																							 {"sClass":"editable", "sType":"numeric"},
 																							 {"sType":"numeric"},
 																							 {"sType":"editable_check"},
 																							 {"sType":"editable_check"},
 
 																						 ],
-																						 "fnDrawCallback": function () {
+																						 "fnDrawCallback":function() {
 																							 $('#itemDetails').find('.editable')
 																								.editable('../items/editable_ajax.php', {
-																														"callback": function(sValue, y) {
+																														"callback":function(sValue, y) {
 																															/* Redraw the table from the new data on the server */
 																															oTable.fnDraw();
 																														},
-																														"submitdata": function(value, message) {
+																														"submitdata":function(value, message) {
 																															return {
-																																"row_id": this.parentNode.getAttribute('id'),
-																																"column": oTable.fnGetPosition(this)[2]
+																																"row_id":this.parentNode.getAttribute('id'),
+																																"column":oTable.fnGetPosition(this)[2]
 																															}
 																														},
-																														"height": "14px"
+																														"height":"14px"
 																													}).end().find('.editable_textarea')
 																								.editable('../items/editable_ajax.php',
-																													{"submitdata": function(value, message) {
+																													{"submitdata":function(value, message) {
 																														return {
-																															"row_id": this.parentNode.getAttribute('id'),
-																															"column": oTable.fnGetPosition(this)[2]
+																															"row_id":this.parentNode.getAttribute('id'),
+																															"column":oTable.fnGetPosition(this)[2]
 
 
 																														}
 																													},
-																														"callback": function(value, y) {
+																														"callback":function(value, y) {
 																															/* Redraw the table from the new data on the server */
 																															oTable.fnDraw();
 																														},
-																														type: 'textarea',
-																														cancel		: 'Cancel',
-																														submit		: 'OK'
+																														type:'textarea',
+																														cancel:'Cancel',
+																														submit:'OK'
 																													}).end().find('.editable_check')
 																								.editable('../items/editable_ajax.php',
 																													{
-																														"callback": function(sValue, y) {
+																														"callback":function(sValue, y) {
 																															/* Redraw the table from the new data on the server */
 																															oTable.fnDraw();
 																														},
-																														type: 'checkbox'
+																														type:'checkbox'
 																													});
 																						 }
 																					 });
