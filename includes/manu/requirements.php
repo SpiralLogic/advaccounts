4f18e9175e0520c27a8c4dcd901a83bf37a28407
unknown <Complex@.(none)>
2011-11-18 05:40:30 +1100

    manufactuing autoploads

---
+++ includes/manu/requirements.php
@@ -0,0 +1,64 @@
+<?php
+	/**********************************************************************
+	Copyright (C) FrontAccounting, LLC.
+	Released under the terms of the GNU General Public License, GPL,
+	as published by the Free Software Foundation, either version 3
+	of the License, or (at your option) any later version.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	See the License here <http://www.gnu.org/licenses/gpl-3.0.html>.
+	 ***********************************************************************/
+	class WO_Requirements
+	{
+		public static function get($woid)
+		{
+			$sql = "SELECT wo_requirements.*, stock_master.description,
+		stock_master.mb_flag,
+		locations.location_name,
+		workcentres.name AS WorkCentreDescription FROM
+		(wo_requirements, locations, " . "workcentres) INNER JOIN stock_master ON
+		wo_requirements.stock_id = stock_master.stock_id
+		WHERE workorder_id=" . DB::escape($woid) . "
+		AND locations.loc_code = wo_requirements.loc_code
+		AND workcentres.id=workcentre";
+			return DB::query($sql, "The work order requirements could not be retrieved");
+		}
+
+		//--------------------------------------------------------------------------------------
+		public static function add($woid, $stock_id)
+		{
+			// create Work Order Requirements based on the bom
+			$result = Manufacturing::get_bom($stock_id);
+			while ($myrow = DB::fetch($result)) {
+				$sql = "INSERT INTO wo_requirements (workorder_id, stock_id, workcentre, units_req, loc_code)
+			VALUES (" . DB::escape($woid) . ", '" . $myrow["component"] . "', '" . $myrow["workcentre_added"] . "', '" . $myrow["quantity"] . "', '" . $myrow["loc_code"] . "')";
+				DB::query($sql, "The work order requirements could not be added");
+			}
+		}
+
+		//--------------------------------------------------------------------------------------
+		public static function delete($woid)
+		{
+			$sql = "DELETE FROM wo_requirements WHERE workorder_id=" . DB::escape($woid);
+			DB::query($sql, "The work order requirements could not be deleted");
+		}
+
+		//--------------------------------------------------------------------------------------
+		public static function update($woid, $stock_id, $quantity)
+		{
+			$sql = "UPDATE wo_requirements SET units_issued = units_issued + " . DB::escape($quantity) . "
+		WHERE workorder_id = " . DB::escape($woid) . " AND stock_id = " . DB::escape($stock_id);
+			DB::query($sql, "The work requirements issued quantity couldn't be updated");
+		}
+
+		//--------------------------------------------------------------------------------------
+		public static function void($woid)
+		{
+			$sql = "UPDATE wo_requirements SET units_issued = 0 WHERE workorder_id = " . DB::escape($woid);
+			DB::query($sql, "The work requirements issued quantity couldn't be voided");
+		}
+		//--------------------------------------------------------------------------------------
+	}
+
+?>
