7b478ceb53dc7c815fe983db5ed1df8162b0dfbe
unknown <Complex@.(none)>
2011-11-28 20:22:22 +1100

    made ui bit autoloading

---
+++ includes/app/apps/advanced.php
@@ -0,0 +1,27 @@
+<?php
+
+	/* * ********************************************************************
+	 Copyright (C) Advanced Group PTY LTD
+	 Released under the terms of the GNU General Public License, GPL,
+	 as published by the Free Software Foundation, either version 3
+	 of the License, or (at your option) any later version.
+	 This program is distributed in the hope that it will be useful,
+	 but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	 See the License here <http://www.gnu.org/licenses/gpl-3.0.html>.
+	* ********************************************************************* */
+	class Apps_Advanced extends Application
+	{
+		function __construct()
+			{
+				parent::__construct("Advanced", _($this->help_context = "&Advanced"));
+				$this->add_module(_("Websales To Jobsboard"));
+				$this->add_lapp_function(0, _("Put websales on Bobs Joard"), "/jobsboard/websales/", 'SA_OPEN');
+				$this->add_lapp_function(0, _("Put websales into accouting"), "/advanced/websales/", 'SA_OPEN');
+				$this->add_lapp_function(0, _("Reload Config"), $_SERVER['PHP_SELF'] . "?reload_config=1", 'SA_OPEN');
+				$this->add_rapp_function(0, "Add To Order from Website",
+					'javascript:var%20s,b=document.getElementsByTagName("body")[0];if(!window.jQuery){s=document.createElement("script");s.setAttribute("src","http://jquery.com/src/jquery-latest.js");b.appendChild(s)};s=document.createElement("script");s.setAttribute("src","https://advanced.sorijen.net.au:2223/js/js2/addfromsite.js?"+Math.floor(Math.random()*1000000));b.appendChild(s);;void(s);',
+					'SA_OPEN');
+				$this->add_rapp_function(0, "New Message", '/messages/messages.php', 'SA_OPEN');
+			}
+	}
---
+++ includes/core/db/query/where.php
@@ -0,0 +1,103 @@
+<?php
+	/**
+	 * Created by JetBrains PhpStorm.
+	 * User: Complex
+	 * Date: 14/08/11
+	 * Time: 11:13 PM
+	 * To change this template use File | Settings | File Templates.
+	 */
+	abstract class DB_Query_Where
+	{
+		public $data = array();
+		protected $where = array();
+		private $wheredata = array();
+		protected $count = 0;
+
+		/***
+		 * @param        $conditions
+		 * @param string $type
+		 * @param null   $uservar
+		 *
+		 * @return DB_Query_Select
+		 */
+		protected function _where($conditions, $type = 'AND', $uservar = null)
+			{
+				if (is_array($conditions)) {
+					foreach ($conditions as $condition) {
+						if (is_array($condition)) {
+							$this->_where($condition[0], $type, $condition[1]);
+						} else {
+							$this->_where($condition);
+						}
+					}
+					return $this;
+				}
+				if ($uservar !== null) {
+					$name = ':dbcondition' . $this->count;
+					$this->count++;
+					$this->wheredata[$name] = $uservar;
+					$conditions = $conditions . ' ' . $name;
+				}
+				$this->where[] = (empty($this->where)) ? $conditions : $type . ' ' . $conditions;
+				return $this;
+			}
+
+		public function where($condition, $uservar = null)
+			{
+				return $this->_where($condition, 'AND', $uservar);
+			}
+
+		public function or_where($condition, $uservar = null)
+			{
+				return $this->_where($condition, 'OR', $uservar);
+			}
+
+		public function and_where($condition, $uservar = null)
+			{
+				return $this->_where($condition, 'AND', $uservar);
+			}
+
+		public function or_open($condition, $uservar = null)
+			{
+				return $this->_where($condition, 'OR (', $uservar);
+			}
+
+		public function and_open($condition, $uservar = null)
+			{
+				return $this->_where($condition, 'AND (', $uservar);
+			}
+
+		public function close_and($condition, $uservar = null)
+			{
+				return $this->_where($condition, ') AND', $uservar);
+			}
+
+		public function close_or($condition, $uservar = null)
+			{
+				return $this->_where($condition, ') OR', $uservar);
+			}
+
+		public function open($condition, $uservar = null)
+			{
+				if (empty($this->where)) {
+					$condition = '(' . $condition;
+				}
+				return $this->_where($condition, ' AND ', $uservar);
+			}
+
+		public function close()
+			{
+				array_push($this->where, array_pop($this->where) . ') ');
+				return $this;
+			}
+
+		protected function _buildWhere()
+			{
+				$sql = '';
+				if (!empty($this->where)) {
+					$sql .= ' WHERE ' . implode(' ', $this->where);
+				}
+				$this->data = $this->data + $this->wheredata;
+				return $sql;
+			}
+	}
