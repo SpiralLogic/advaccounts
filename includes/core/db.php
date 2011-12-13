4a05c2eb9117e5e36e03b9b275f0f5d3d90ae0f1
Advanced <admin@advancedgroup.com.au>
2011-12-13 14:13:17 +1100

    JSONENCOFINH

--- includes/core/db.php
+++ includes/core/db.php
@@ -6,8 +6,7 @@
 	 * Time: 4:41 AM
 	 * To change this template use File | Settings | File Templates.
 	 */
-	class DB
-	{
+	class DB {
 		/**
 		 *
 		 */
@@ -31,7 +30,6 @@
 		/**
 		 * @var bool
 		 */
-		protected static $current = false;
 		/**
 		 * @var array
 		 */
@@ -62,12 +60,8 @@
 		 * @return DB_Connection
 		 */
 		protected static function _get($conn = null, $config = array()) {
-			static::$prepared = null;
-			if ($conn === null && static::$current) {
-				return static::$current;
-			} elseif ($conn === null) {
+			if ($conn === null ) {
 				$config = $config ? : Config::get('db_default');
-				$conn = $config['name'];
 			}
 			if (!isset(static::$connections[$conn])) {
 				static::$connections[$conn] = static::$current = DB_Connection::i($conn, $config);
@@ -348,7 +342,7 @@
 		 */
 		public static function error_msg() {
 			$info = static::_get()->errorInfo();
-return $info;
+			return $info;
 		}
 
 		/**
@@ -441,16 +435,280 @@ return $info;
 		 * @param $key
 		 */
 		public static function update_record_status($id, $status, $table, $key) {
-		$reuslt = 	DB::update($table)->value('inactive',$status)->where($key.'=',$id)->exec();
+			$reuslt = DB::update($table)->value('inactive', $status)->where($key . '=', $id)->exec();
 			if (!$reuslt) {
 				static::insert_record_status($id, $status, $table, $key);
 			}
 			return $reuslt;
 		}
+
 		public static function insert_record_status($id, $status, $table, $key) {
 
-			$reuslt = DB::insert($table)->values(array('inactive'=>$status,$key=>$id))->exec();
+			$reuslt = DB::insert($table)->values(array('inactive' => $status, $key => $id))->exec();
 
-				return $reuslt;
+			return $reuslt;
+		}
+	}
+
+	/**
+	 * Created by JetBrains PhpStorm.
+	 * User: Complex
+	 * Date: 21/08/11
+	 * Time: 11:15 PM
+	 * To change this template use File | Settings | File Templates.
+	 */
+	class DB_Connection {
+		/**
+		 * @var array
+		 */
+		protected static $instances = array();
+
+		/**
+		 * @static
+		 *
+		 * @param $name
+		 * @param $config
+		 *
+		 * @return mixed
+		 */
+
+		/**
+		 * @var
+		 */
+		protected $name;
+		/**
+		 * @var
+		 */
+		protected $user;
+		/**
+		 * @var
+		 */
+		protected $pass;
+		/**
+		 * @var
+		 */
+		protected $host;
+		/**
+		 * @var
+		 */
+		protected $port;
+		/**
+		 * @var PDO
+		 */
+		protected $conn;
+		/**
+		 * @var bool
+		 */
+		protected $intransaction = false;
+
+		/**
+		 * @param			 $name
+		 * @param array $config
+		 */
+		protected function __construct($name, array $config) {
+			$this->name = $name;
+			$this->user = $config['user'];
+			$this->pass = $config['pass'];
+			$this->host = $config['host'];
+			$this->port = $config['port'];
+			$this->debug = false;
+			$this->_connect();
+			static::$instances[$name] = $this;
+		}
+
+		/**
+		 * @return mixed
+		 */
+		public function name() {
+			return $this->name;
+		}
+
+		/**
+		 * @param $sql
+		 *
+		 * @return PDOStatement
+		 *
+		 */
+		public function prepare($sql) {
+			try {
+				return $this->conn->prepare($sql);
 			}
+			catch (PDOException $e) {
+				$this->_error($e);
+			}
+		}
+
+		/***
+		 * @param			$sql
+		 * @param			$type
+		 * @param null $data
+		 *
+		 * @return DB_Query_Result|int
+		 */
+		public function exec($sql, $type, $data = null) {
+			try {
+				$prepared = $this->prepare($sql);
+				switch ($type) {
+					case DB::SELECT:
+						return new DB_Query_Result($prepared, $data);
+					case DB::INSERT:
+						$prepared->execute($data);
+						return $this->conn->lastInsertId();
+					case DB::UPDATE or DB::DELETE:
+						$prepared->execute($data);
+						return true;
+				}
+			}
+			catch (PDOException $e) {
+				$this->_error($e);
+			}
+			return false;
+		}
+
+		/**
+		 * @return DB_Connection
+		 */
+		public function begin() {
+			if ($this->intransaction == true) {
+				return $this;
+			}
+			try {
+				$this->conn->beginTransaction();
+			}
+			catch (PDOException $e) {
+				static::_error($e);
+			}
+			$this->intransaction = true;
+			return $this;
+		}
+
+		/**
+		 * @return mixed
+		 */
+		public function lastInsertId() {
+			return $this->conn->lastInsertId();
+		}
+
+		/**
+		 * @return DB_Connection
+		 */
+		public function commit() {
+			if ($this->intransaction == false) {
+				return $this;
+			}
+			try {
+				$this->conn->commit();
+			}
+			catch (PDOException $e) {
+				static::_error($e);
+			}
+			$this->intransaction = false;
+			return $this;
+		}
+
+		/**
+		 * @return DB_Connection
+		 */
+		public function cancel() {
+			if ($this->intransaction == false) {
+				return $this;
+			}
+			try {
+				$this->conn->rollBack();
+			}
+			catch (PDOException $e) {
+				static::_error($e);
+			}
+			$this->intransaction = false;
+			return $this;
+		}
+
+		/**
+		 * @param		 $sql
+		 * @param int $fetchas
+		 *
+		 * @return bool
+		 */
+		public function query($sql, $fetchas = PDO::FETCH_OBJ) {
+			try {
+				$query = $this->conn->prepare($sql);
+				if ($fetchas == false) {
+					return $query->execute();
+				}
+				$results = $query->fetchAll($fetchas);
+			}
+			catch (PDOException $e) {
+				return static::_error($e);
+			}
+			return $results;
+		}
+
+		/**
+		 * @param $value
+		 *
+		 * @return mixed
+		 */
+		public function quote($value) {
+			return $this->conn->quote($value);
+		}
+
+		/**
+		 *
+		 */
+		protected function _connect() {
+			try {
+				$this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->name, $this->user, $this->pass, array(PDO::MYSQL_ATTR_FOUND_ROWS => true));
+				$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			}
+			catch (PDOException $e) {
+				return $this->_error($e, true);
+			}
+		}
+
+		/**
+		 * @return mixed
+		 */
+		public function errorCode() {
+			return $this->conn->errorCode();
+		}
+
+		/**
+		 * @return mixed
+		 */
+		public function errorInfo() {
+			return $this->conn->errorInfo();
+		}
+
+		/**
+		 * @param PDO|int $value
+		 *
+		 * @return mixed
+		 */
+		public function getAttribute(PDO $value) {
+			return $this->conn->getAttribute($value);
+		}
+
+		/**
+		 * @param PDOException $e
+		 * @param bool				 $exit
+		 *
+		 * @return bool
+		 * @throws DB_Exception
+		 */
+		protected function _error(PDOException $e, $exit = false) {
+			if (Config::get('debug_sql')) {
+				$error = '<p>DATABASE ERROR: <pre>' . '</pre></p><p><pre></pre></p>';
+			} else {
+				$error = $e->errorInfo;
+				$error = (!isset($error[2])) ? $e->getMessage() : $error[2];
+			}
+			if ($this->conn->inTransaction()) {
+				$this->conn->rollBack();
+				$this->intransaction = false;
+			}
+			if ($exit) {
+				throw new DB_Exception($error);
+			}
+			Errors::show_db_error($error);
+		}
 	}
