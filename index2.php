f52febc330262bc138773bea1caac6750ef0e791
unknown <Complex@.(none)>
2011-12-28 08:33:28 +1100

    changelist

--- index2.php
+++ index2.php
@@ -7,14 +7,27 @@
 	 * To change this template use File | Settings | File Templates.
 	 */
 	include ('bootstrap.php');
-	$items = DB::select('stock_id', 'description')->from('stock_master')->limit(0, 200)->fetch()->all();
-	foreach ($items as $item) {
+	$result = DB::select('stock_id', 'description')->from('stock_master')->where("concat('',stock_id * 1) <> stock_id")->fetch();
+	echo count($result);
+	foreach ($result as $item) {
+		$data = Barcode::create(array('code' => $item['stock_id'] . "\n" . $item['description']));
+		$image = base64_encode($data);
+		echo '<div style="width:25%; text-align:center;float:left"><IMG SRC="data:image/gif;base64,' . $image . '">' . '<br>' . $item['stock_id'] . '<br>' . $item['description'] . '</div>';
+		$result->next();
+		$item = $result->current();
+		$data = Barcode::create(array('code' => $item['stock_id'] . "\n" . $item['description']));
+		$image = base64_encode($data);
+		echo '<div style="width:25%; text-align:center;float:left"><IMG SRC="data:image/gif;base64,' . $image . '">' . '<br>' . $item['stock_id'] . '<br>' . $item['description'] . '</div>';
+		$result->next();
+		$item = $result->current();
+		$data = Barcode::create(array('code' => $item['stock_id'] . "\n" . $item['description']));
+		$image = base64_encode($data);
+		echo '<div style="width:25%; text-align:center;float:left"><IMG SRC="data:image/gif;base64,' . $image . '">' . '<br>' . $item['stock_id'] . '<br>' . $item['description'] . '</div>';
+		$result->next();
+		$item = $result->current();
 		$data = Barcode::create(array('code' => $item['stock_id'] . "\n" . $item['description']));
 		$image = base64_encode($data);
 		echo '<div style="display:inline-block;text-align:center;clear:left;width:25%;"><IMG SRC="data:image/gif;base64,' . $image . '">' . '<br>' . $item['stock_id'] . '<br>' . $item['description'] . '</div>';
-		echo '<div style="width:25%; text-align:center;float:left"><IMG SRC="data:image/gif;base64,' . $image . '">' . '<br>' . $item['stock_id'] . '<br>' . $item['description'] . '</div>';
-		echo '<div style="width:25%; text-align:center;float:left"><IMG SRC="data:image/gif;base64,' . $image . '">' . '<br>' . $item['stock_id'] . '<br>' . $item['description'] . '</div>';
-		echo '<div style="width:25%; text-align:center;float:left"><IMG SRC="data:image/gif;base64,' . $image . '">' . '<br>' . $item['stock_id'] . '<br>' . $item['description'] . '</div>';
 	}
 
 ?>
