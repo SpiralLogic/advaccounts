9d06306585622cb536d71fc55a1a1f26279b484d
Sol Jennings <Maidenii@cpe-124-179-86-119.lns6.cht.bigpond.net.au>
2012-01-07 03:40:35 +1100

    converted the rest of mode and selected id to non glboals

--- advanced/websales/index.php
+++ advanced/websales/index.php
@@ -3,6 +3,6 @@
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/bootstrap.php");
 	$page_security = SA_OPEN;
 
-	Page::start(_($help_context = "Websales to Accounting"), Input::request('frame'));
+	Page::start(_($help_context = "Websales to Accounting"));
 	Page::end();
 ?>
