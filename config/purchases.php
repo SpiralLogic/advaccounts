ec34a508182be6c375eb54f7ef9d518dec824ecc
Maidenii <sol@sorijen.com.au>
2012-02-29 15:41:53 +1100

    still used namespace report changed back for now

---
+++ config/purchases.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Maidenii
+ * Date: 29/02/12
+ * Time: 3:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+return array(
+	'purchases.valid_charged_to_delivered_qty' => true, // check price charged on invoices exceeds the purchaseorder price.
+	'valid_charged_to_delivered_price' => true, //
+	);
