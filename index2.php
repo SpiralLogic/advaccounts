4b9b3529d35dbecc252e2bb74bab57791c1cc550
Advanced <admin@advancedgroup.com.au>
2012-03-14 15:09:08 +1100

    fixed empty index in bank, 						fixed company login missing in post again, 						fixed no error if website cant be  contacted for putting shit on jobdsboard

--- index2.php
+++
@@ -1,9 +0,0 @@
-<?php
-  /**
-   * Created by JetBrains PhpStorm.
-   * User: Maidenii
-   * Date: 25/02/12
-   * Time: 1:52 PM
-   * To change this template use File | Settings | File Templates.
-   */
-  include('bootstrap.php');
