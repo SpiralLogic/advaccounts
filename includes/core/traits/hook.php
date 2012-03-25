08fdceebde0b644715aa90479314916945930a3d
Spiral Logic <sol@advancedgroup.com.au>
2012-03-25 15:42:09 +1100

    added hooks/hooktraits and autoloading for traits

---
+++ includes/core/traits/hook.php
@@ -0,0 +1,25 @@
+<?php
+	/**
+	 * PHP version 5.4
+	 * @category  PHP
+	 * @package   ADVAccounts
+	 * @author    Advanced Group PTY LTD <admin@advancedgroup.com.au>
+	 * @copyright 2010 - 2012
+	 * @link      http://www.advancedgroup.com.au
+	 **/
+
+	trait HookTrait {
+
+		/** @var Hook */
+		public static $hooks = null;
+		public static function _register($hook, $object, $function, $arguments = array()) {
+			if (self::$hooks === null) {
+				self::$hooks = new Hook();
+			}
+			$callback = $object . '::' . $function;
+			if (!is_callable($callback)) {
+				throw new HookException("Class $object doesn't have a callable function $function");
+			}
+			self::$hooks->add($hook, $callback, $arguments);
+		}
+	}
