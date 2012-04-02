841c7978f00935b9c3da4092f4837b36438b51c9
Spiral Logic <sol@advancedgroup.com.au>
2012-04-02 14:44:43 +1000

    fuck

--- includes/core/autoloader.php
+++ includes/core/autoloader.php
@@ -184,7 +184,7 @@
           $alias = TRUE;
         }
         $namespacepath = str_replace(['\\', 'ADV'], [DS, 'includes'], $namespace);
-        $dir = DOCROOT . strtolower($namespacepath);
+        $dir = DOCROOT . strtolower($namespacepath.DS.$filename);
       }
       elseif (isset(static::$classes[$required_class])) {
         $dir = rtrim(static::$classes[$required_class], '/') . DS . $filename;
