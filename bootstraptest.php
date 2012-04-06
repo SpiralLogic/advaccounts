d7156989d8b5172ef281a3a871f84a954e258640
Maidenii <sol@sorijen.com.au>
2012-04-06 14:20:12 +1000

    namespaces finished

--- bootstraptest.php
+++ bootstraptest.php
@@ -1,17 +1,13 @@
 <?php
   /**
    * PHP version 5.4
-   * @category  PHP
+  \   * @category  PHP
    * @package   ADVAccounts
    * @author    Advanced Group PTY LTD <admin@advancedgroup.com.au>
    * @copyright 2010 - 2012
    * @link      http://www.advancedgroup.com.au
    **/
-  include_once('PHPUnit/Autoload.php');
-  error_reporting(-1);
-  ini_set('display_errors', 1);
-  ini_set("ignore_repeated_errors", "On");
-  ini_set("log_errors", "On");
+
   define('E_SUCCESS', E_ALL << 1);
   define('DS', DIRECTORY_SEPARATOR);
   define('DOCROOT', __DIR__ . DS);
@@ -20,38 +16,49 @@
   define('VENDORPATH', DOCROOT . 'includes' . DS . 'vendor' . DS);
   define("AJAX_REFERRER", (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'));
   define('IS_JSON_REQUEST', (isset($_SERVER['HTTP_ACCEPT']) && strpos($_SERVER['HTTP_ACCEPT'], 'application/json') !== FALSE));
+  define('BASE_URL', str_ireplace(realpath(__DIR__), '', DOCROOT));
   define('CRLF', chr(13) . chr(10));
-  define('PATH_TO_ROOT', substr(str_repeat('..' . DS, substr_count(BASE_URL . DS, DS)), 0, -1) ? : '.');
-  define('MBSTRING', function_exists('mb_get_info'));
+  define('PATH_TO_ROOT', substr(str_repeat('..' . DS, substr_count(str_replace(DOCROOT, '', realpath('.') . DS), DS)), 0, -1) ? :
+    '.');
   set_error_handler(function ($severity, $message, $filepath, $line) {
-    (!class_exists('Errors', FALSE)) and include(COREPATH . 'errors.php');
-    return \Errors::handler($severity, $message, $filepath, $line);
+    class_exists('Adv\\Core\\Errors', FALSE) or include COREPATH . 'errors.php';
+    return ADV\Core\Errors::handler($severity, $message, $filepath, $line);
   });
   set_exception_handler(function (\Exception $e) {
-    (!class_exists('Errors', FALSE)) and  include(COREPATH . 'errors.php');
-    \Errors::exception_handler($e);
+    var_dump(class_exists('\\ADV\\Core\\Errors', FALSE));
+    class_exists('ADV\\Core\\Errors', FALSE) or include COREPATH . 'errors.php';
+    ADV\Core\Errors::exception_handler($e);
   });
+  require COREPATH . 'autoloader.php';
   if (!function_exists('e')) {
+    /**
+     * @param $string
+     *
+     * @return array|string
+     */
     function e($string) { return Security::htmlentities($string); }
   }
-  require COREPATH . 'autoloader.php';
   register_shutdown_function(function () {
-    if (!class_exists('Event', FALSE)) {
-      include(COREPATH . 'event.php');
-    }
-    \Event::shutdown();
+    class_exists('Event', FALSE) or  include(COREPATH . 'event.php');
+    Event::shutdown();
   });
   if (!function_exists('adv_ob_flush_handler')) {
     /**
      * @param $text
      *
      * @return string
+     * @noinspection PhpUnusedFunctionInspection
      */
     function adv_ob_flush_handler($text) {
       return (Ajax::i()->in_ajax()) ? Errors::format() : Errors::$before_box . Errors::format() . $text;
     }
   }
-  Config::i();
-  include(DOCROOT . 'config' . DS . 'defines.php');
+  define('COMPANY_PATH' ,DOCROOT. 'company' . DS);
+
+  Cache::define_constants('defines', function() {
+    return include(DOCROOT . 'config' . DS . 'defines.php');
+  });
   include(DOCROOT . 'config' . DS . 'types.php');
   include(DOCROOT . 'config' . DS . 'access_levels.php');
+  Session::i();
+  Config::i();
