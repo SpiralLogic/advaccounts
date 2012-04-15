01cf2acb8a015b614378ec869a9cad8bf3828d72
Maidenii <sol@sorijen.com.au>
2012-04-15 11:14:10 +1000

    auto-generated documentation

---
+++ web/tmp/idestorm/IDE.php
@@ -0,0 +1,87 @@
+<?php
+  /**
+     * PHP version 5.4
+     * @category  PHP
+     * @package   ADVAccounts
+     * @author    Advanced Group PTY LTD <admin@advancedgroup.com.au>
+     * @copyright 2010 - 2012
+     * @link      http://www.advancedgroup.com.au
+     **/
+  class Arr extends ADV\Core\Arr {
+  }
+
+  class Adv_Exception extends ADV\Core\Adv_Exception {
+  }
+
+  class Ajax extends ADV\Core\Ajax {
+  }
+
+  class Auth extends ADV\Core\Auth {
+  }
+
+  class Cache extends ADV\Core\Cache {
+  }
+
+  class Config extends ADV\Core\Config {
+  }
+
+  class Dates extends ADV\Core\Dates {
+  }
+
+  class DB extends ADV\Core\DB\DB {
+  }
+
+  class Query_Result extends ADV\Core\DB\Query_Result {
+  }
+
+  class Dialog extends ADV\Core\Dialog {
+  }
+
+  class Event extends ADV\Core\Event {
+  }
+
+  class Errors extends ADV\Core\Errors {
+  }
+
+  class Files extends ADV\Core\Files {
+  }
+
+  class HTML extends ADV\Core\HTML {
+  }
+
+  class Hook extends ADV\Core\Hook {
+  }
+
+  class Input extends ADV\Core\Input {
+
+  }
+
+  class JS extends ADV\Core\JS {
+  }
+
+  class Language extends ADV\Core\Language {
+  }
+
+  class Menu extends ADV\Core\Menu {
+  }
+
+  class MenuUI extends ADV\Core\MenuUI {
+  }
+
+  class Num extends ADV\Core\Num {
+  }
+
+  class Session extends ADV\Core\Session {
+  }
+
+  class Status extends ADV\Core\Status {
+  }
+
+  class UploadHandler extends ADV\Core\UploadHandler {
+  }
+class DBException extends ADV\Core\DB\DBException{}
+class DBSelectException extends ADV\Core\DB\DBException{}
+class DBUpdateException extends ADV\Core\DB\DBException{}
+class DBDeleteException extends ADV\Core\DB\DBException{}
+class DBInsertException extends ADV\Core\DB\DBException{}
+
