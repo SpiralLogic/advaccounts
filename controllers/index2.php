972c975eb6edea2ce7d376026995ed1e9253d00e
Advanced Admin <advadmin@advanced.sorijen.net.au>
2012-05-10 01:42:03 +1000

    ADVA-1 does it work

--- controllers/index2.php
+++ controllers/index2.php
@@ -2,25 +2,5 @@
 /*  if (!User::i()->can_access(SA_DEBUGGING)) {
     throw new Adv_Exception("Administrator access only");
   }*/
-  /*  echo '<pre>';
-  $sql
-    = "SELECT reconciled,bank_act FROM bank_trans
-               WHERE bank_act=13 AND reconciled IS NOT NULL AND amount!=0
-               GROUP BY reconciled";
-  $results = DB::query($sql)->fetchAll();
-  foreach ($results as $row) {
-    $recon = GL_Account::get_max_reconciled(Dates::sql2date($row['reconciled']), $row['bank_act'])->fetch();
-    $values = array(
-      'bank_act' => $row['bank_act'],
-      'begin_balance' => $recon['beg_balance'],
-      'end_balance' => $recon['end_balance'],
-      'date' => $row['reconciled']
-    );
-    try {
-      DB::insert('bank_reconciles')->values($values)->exec();
-    }
-    catch (ADV\Core\DB\DBDuplicateException $e) {
-      continue;
-    }
-  }*/
+//todo remove
   phpinfo();
