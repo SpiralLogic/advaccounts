c20c38182576b98c307ec40c35b65909653195d9
Advanced <admin@advancedgroup.com.au>
2012-05-24 12:00:18 +1000

    sdf

--- config/email.php
+++ config/email.php
@@ -11,7 +11,7 @@
     'server' => 'mx2.sorijen.net.au', //
     'port' => 25, //
     'username' => 'sales@advancedroadsigns.com.au', //
-    'password' => '14ntigone24ntigone', //
+    'password' => '1Ant1berg', //
     'from_email' => 'sales@advancedroadsigns.com.au', //
     'from_name' => 'Advanced Group Accounts', //
     'bcc' => 'sales@advancedroadsigns.com.au'
--- controllers/index2.php
+++ controllers/index2.php
@@ -1,10 +1,1 @@
 <?php
-  /*  if (!User::i()->can_access(SA_DEBUGGING)) {
-    throw new Adv_Exception("Administrator access only");
-  }*/
-//todo remove
-$test = new Debtor();
-$test->name ='wwawawawawawa';
-$test->save();
-var_dump($test);
-
