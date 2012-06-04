7b18d14af35e401a56f7ca19b67ce3ee130e4097
Spiral Logic <sol@advancedgroup.com.au>
2012-06-04 11:09:53 +1000

    make forms static

--- classes/core/dates.php
+++ classes/core/dates.php
@@ -519,7 +519,7 @@
         $months[$i] = date('F', strtotime("now - $i months"));
       }
 
-      return Form::arraySelect($name, $month, $months);
+      return \Form::arraySelect($name, $month, $months);
     }
     /**
      * @static
