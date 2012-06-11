0ffed74d3e5a5a31502119d38fe1763919f1c33c
Spiral Logic <sol@advancedgroup.com.au>
2012-06-11 20:06:11 +1000

    added twig?

---
+++ views/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html {{ class }} dir='{{ lang_dir }}' >
+<head>    <meta charset='utf-8'>
+<title>{{ title }}</title><script>document.documentElement.className = document.documentElement.className +' js'</script>
+{{css}}
+<link rel='apple-touch-icon' href='/company/images/Advanced-Group-Logo.png'/>
+{{ scripts }}
+</head><body {{ $body_class }}>
+<div id='content'>
