137adbe1ca640509382b9eee6f7dd2e5845f497d
Maidenii <sol@sorijen.com.au>
2012-09-22 18:30:15 +1000

    miving controllers

---
+++ classes/App/Controller/Menu.php
@@ -0,0 +1,14 @@
+<?php
+  /**
+   * PHP version 5.4
+   *
+   * @category  PHP
+   * @package   ADVAccounts
+   * @author    Advanced Group PTY LTD <admin@advancedgroup.com.au>
+   * @copyright 2010 - 2012
+   * @date      22/09/12
+   * @link      http://www.advancedgroup.com.au
+   *
+   **/
+  class Menu {
+  }
