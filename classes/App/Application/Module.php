4ee3cd2f6e198c077d3b06ded30dae074ebbb846
Advanced <admin@advancedgroup.com.au>
2012-11-12 20:24:10 +1100

    changed line seperators

--- classes/App/Application/Module.php
+++ classes/App/Application/Module.php
@@ -1,28 +1,26 @@
 <?php
+  /**
+   * Created by JetBrains PhpStorm.
+   * User: Complex
+   * Date: 8/07/12
+   * Time: 4:50 AM
+   * To change this template use File | Settings | File Templates.
+   */
+  namespace ADV\App\Application;
+
+  /**
+
+   */
+  class Module
+  {
     /**
-     * Created by JetBrains PhpStorm.
-     * User: Complex
-     * Date: 8/07/12
-     * Time: 4:50 AM
-     * To change this template use File | Settings | File Templates.
+     * @param      $name
+     * @param null $icon
      */
-    namespace ADV\App\Application;
-
-    /**
-
-     */
-    class Module
-    {
-      /**
-         * @param      $name
-         * @param null $icon
-         */
-        public function __construct($name, $icon = null)
-        {
-            $this->name              = $name;
-            $this->icon              = $icon;
-            $this->leftAppFunctions  = [];
-            $this->rightAppFunctions = [];
-        }
-
+    public function __construct($name, $icon = null) {
+      $this->name              = $name;
+      $this->icon              = $icon;
+      $this->leftAppFunctions  = [];
+      $this->rightAppFunctions = [];
     }
+  }
