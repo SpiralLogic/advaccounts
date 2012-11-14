30d192e9ea887b5b69b5de8f4f42eec796ec6781
Maidenii <admin@advancedgroup.com.au>
2012-11-14 22:32:25 +1100

    statements with deposits

--- controllers/reporting/rep108.php
+++ controllers/reporting/rep108.php
@@ -30,7 +30,7 @@
     $dateend   = date('Y-m-d', mktime(0, 0, 0, date('n') - $month, 0));
     $datestart = date('Y-m-d', mktime(0, 0, 0, date('n') - $month - 1, 1));
     $sql       = "SELECT d.*," . "((d.ov_amount + d.ov_gst + d.ov_freight + d.ov_freight_tax + d.ov_discount) * IF(d.type = " . ST_SALESINVOICE . " OR d.type = " . ST_BANKPAYMENT . ",1,-1) ) AS TotalAmount,";
-    $sql .= "IF(d.type <> 10,-SUM(d.alloc), if(a.amt is null,0,SUM(a.amt))*if(b.tran_date > '$dateend',0,1)) AS Allocated, ";
+    $sql .= "IF(d.type <> 10 AND d.type <> 1,-SUM(d.alloc), if(a.amt is null,0,SUM(a.amt))*if(b.tran_date > '$dateend',0,1)) AS Allocated, ";
     $sql
       .= "( d.due_date < '$datestart' AND (b.tran_date < '$datestart' or b.tran_date is null)) AS OverDue
                             FROM debtor_trans d
@@ -176,7 +176,7 @@ CONCAT(a.br_address,CHARACTER(13),a.city," ",a.state," ",a.postcode) as address 
       }
       foreach ($transactions as $i => $trans) {
         if (!$inc_payments && in_array($trans['type'], [ST_CUSTPAYMENT, ST_BANKDEPOSIT])) {
-          continue;
+          //  continue;
         }
         $display_total       = Num::_format(abs($trans["TotalAmount"]), $dec);
         $outstanding         = abs($trans["TotalAmount"] - $trans["Allocated"]);
