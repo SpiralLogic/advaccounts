184a957f2611456335cba142acac27c1a84588e9
Maidenii <admin@advancedgroup.com.au>
2012-12-14 13:21:22 +1100

    fixed tax adding to tax

--- config/assets.php
+++ config/assets.php
@@ -2,27 +2,27 @@
   /*
   * SmartOptimizer Configuration File
   */
-  return array(
-    'css'          => array(
+  return [
+    'css'          => [
       'default.css', //
       'jquery-ui.css', //
       'jquery.calc.css', //
       'font-awesome.css', //
-    ),
+    ],
     //
-    'footer'       => array(
+    'footer'       => [
       '/js/libs/JsHttpRequest.js',
       '/js/libs/jquery.min.js',
       '/js/libs/jquery-ui.min.js',
       '/js/advanced.js',
       '/js/behaviour.js',
-      '/js/libs/jquery-tmpl.js',
-      '/js/libs/jquery.calc.min.js',
-      '/js/libs/bootstrap.js',
+      '/js/libs/jquery-tmpl.min.js',
+      '/js/libs/jquery.calc.js',
+      '/js/libs/bootstrap.min.js',
       '/js/calc.js',
       '/js/help_modal.js',
-    ),
+    ],
     //
     'maps_api_key' => 'ABQIAAAAi6QUBXugcwuPqyTcPMoi7BRpI0TaLspvsTOnOMM4T78mt8FLYBQC5z4qKk3rJIKFVEWI7RCzzewJLQ',
     //
-  );
+  ];
