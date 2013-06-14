45b4a3862f67363867aaf9f60f9f150b84654a83
Spiral Logic <sol@advancedgroup.com.au>
2013-06-14 11:34:23 +1000

    fixed pager

--- config/assets.php
+++ config/assets.php
@@ -1,29 +1,29 @@
 <?php
-  /*
-  * SmartOptimizer Configuration File
-  */
-  return [
-    'css'          => [
-      'default.css', //
-      'jquery-ui.css', //
-      'jquery.calc.css', //
-      'font-awesome.css', //
-    ],
-    //
-    'footer'       => [
-      '/js/wibs/JsHttpRequest.js',
-      '/js/libs/jquery.min.js',
-      '/js/libs/jquery-ui.min.js',
-      '/js/advanced.js',
-      '/js/behaviour.js',
-      '/js/libs/jquery-tmpl.js',
-      '/js/libs/jquery.calc.min.js',
-      '/js/libs/bootstrap.js',
-      '/js/calc.js',
-      '/js/help_modal.js',
-      '/js/sidemenu.js',
-    ],
-    //
-    'maps_api_key' => 'ABQIAAAAi6QUBXugcwuPqyTcPMoi7BRpI0TaLspvsTOnOMM4T78mt8FLYBQC5z4qKk3rJIKFVEWI7RCzzewJLQ',
-    //
-  ];
+    /*
+    * SmartOptimizer Configuration File
+    */
+    return [
+        'css'          => [
+            'default.css', //
+            'jquery-ui.css', //
+            'jquery.calc.css', //
+            'font-awesome.css', //
+        ],
+        //
+        'footer'       => [
+            '/js/libs/JsHttpRequest.js',
+            '/js/libs/jquery.min.js',
+            '/js/libs/jquery-ui.min.js',
+            '/js/advanced.js',
+            '/js/behaviour.js',
+            '/js/libs/jquery-tmpl.js',
+            '/js/libs/jquery.calc.min.js',
+            '/js/libs/bootstrap.js',
+            '/js/calc.js',
+            '/js/help_modal.js',
+            '/js/sidemenu.js',
+        ],
+        //
+        'maps_api_key' => 'ABQIAAAAi6QUBXugcwuPqyTcPMoi7BRpI0TaLspvsTOnOMM4T78mt8FLYBQC5z4qKk3rJIKFVEWI7RCzzewJLQ',
+        //
+    ];
--- views/ui/dropdown.tpl
+++ views/ui/dropdown.tpl
@@ -1,14 +1,14 @@
 <div class="btn-group {{$auto}}">
-{{#if $split}}
+  {{#if $split}}
     <button class="btn btn-mini btn-primary btn-split">{{$title}}</button>
     <button class='btn btn-mini btn-primary dropdown-toggle {{$auto}}' data-toggle="dropdown"><span class="icon-caret-down"></span></button>
-{{#else}}
+  {{else}}
     <button class="btn btn-mini btn-primary dropdown-toggle {{$auto}}" data-toggle="dropdown">{{$title}}&nbsp;<span class="icon-caret-down"></span></button>
-{{/if}}
-    <ul class="dropdown-menu">
+  {{/if}}
+  <ul class="dropdown-menu">
     {{#$items}}
-        <li>
-            <a {{#$.attr}} {{!}}="{{.}}"{{/$.attr}}>{{$.label}}</a></li>
+      <li>
+        <a {{#$.attr}} {{!}}="{{.}}"{{/$.attr}}>{{$.label}}</a></li>
     {{/$items}}
-    </ul>
+  </ul>
 </div>
