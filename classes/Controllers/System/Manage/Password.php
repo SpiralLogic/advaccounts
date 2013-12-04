6fe6642b641b2ec3617d679c1f4c833738785dc6
Advanced <admin@advancedgroup.com.au>
2013-12-04 23:30:32 +1100

    new password changing

---
+++ classes/Controllers/System/Manage/Password.php
@@ -0,0 +1,12 @@
+<?php
+  /**
+   * Created by PhpStorm.
+   * User: advanced
+   * Date: 4/12/13
+   * Time: 11:30 PM
+   */
+  namespace ADV\Controllers\System\Manage;
+
+  class Password
+  {
+  }
